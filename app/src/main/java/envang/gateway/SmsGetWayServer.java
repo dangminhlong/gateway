98133529b14772e2d48435ffd0a254a5ce01e8e6
dmlon <dangminhlong@gmail.com>
2016-02-02 01:49:56 +0700

    modified

--- app/src/main/java/envang/gateway/SmsGetWayServer.java
+++ app/src/main/java/envang/gateway/SmsGetWayServer.java
@@ -2,6 +2,11 @@ package envang.gateway;
 
 import android.telephony.SmsManager;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -18,16 +23,30 @@ public class SmsGetWayServer extends NanoHTTPD {
 
     @Override
     public Response serve(IHTTPSession session){
+        HashMap<String, String> map = new HashMap<String, String>();
+
         Map<String, String> params = session.getParms();
         String sendTo = params.get("To");
         String sendMsg = params.get("Message");
+        JSONObject jsonObject = new JSONObject();
         try {
             smgr.sendTextMessage(sendTo, null, sendMsg, null, null);
-            return newFixedLengthResponse("{success:true}");
+            try {
+                jsonObject.put("sucess", "true");
+            }
+            catch (Exception ec){
+                return newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "application/json", "{success:true}");
+            }
         }
-        catch (Exception ex){
-            return newFixedLengthResponse("{success:false}");
+        catch (Exception ex) {
+            try {
+                jsonObject.put("sucess", "false");
+            } catch (Exception e) {
+                return newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "application/json","{success:false}");
+            }
         }
-
+        String msg = jsonObject.toString();
+        Response response = newFixedLengthResponse(NanoHTTPD.Response.Status.OK, "application/json", msg);
+        return response;
     }
 }
