541ca9fa6eedc4e6031a232305529e366f9f9cba
Dang Minh Long <dangminhlong@gmail.com>
2016-02-02 16:24:45 +0700

    commit

--- app/src/main/java/envang/gateway/MainActivity.java
+++ app/src/main/java/envang/gateway/MainActivity.java
@@ -3,6 +3,7 @@ package envang.gateway;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -23,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         tv_ipAddress = (TextView)findViewById(R.id.tv_ipAddress);
         cb_runServer = (CheckBox)findViewById(R.id.cb_runServer);
